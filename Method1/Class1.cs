f2a763b04724b1ed8f24b61680ff154eee04747c
mmf09 <mmf09@YPC>
2023-01-03 17:04:49 -0800

    Add project files.

---
+++ Method1/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Method1
+{
+    class Class1
+    { // creating a method with same name but with different types
+        public int Number (int addNumber) // creating a method which will be called from the main program
+        {
+            int total1 = addNumber + 82;
+            return total1;
+
+        }
+        public int Number (decimal multiplyNumber)
+        {
+            int total2 = Convert.ToInt32(multiplyNumber) * 3;
+            return total2;
+        }
+        public int Number(string divideNumber) 
+        {
+            int total2 = Convert.ToInt32(divideNumber); //converting string to integer
+            {
+                int total3 = Convert.ToInt32(divideNumber) / 2;
+                return total3;
+            }
+        }
+    }
+}
---
+++ Method1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Method1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 mathOp = new Class1();//instantiating the class and calling the method
+            Console.WriteLine("Passing an integer 19 and adding 82");
+            Console.WriteLine(mathOp.Number(19));
+            Console.WriteLine("Passing a decimal 1.75 and multiplying by 3"); 
+            Console.WriteLine(mathOp.Number(1.75m));
+            Console.WriteLine("Passing as 20 as a string");
+            Console.WriteLine(mathOp.Number("20"));
+            Console.ReadLine();
+        }
+    }
+}
